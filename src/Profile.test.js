770c0a7bdc32dacb4a20c8763fa60979c5781b64
JaemoKoo <devgoozz@gmail.com>
2023-04-04 23:11:08 +0900

    fix: 테스트 코드 주석 추가

--- src/Profile.test.js
+++ src/Profile.test.js
@@ -9,10 +9,12 @@ describe("<Profile />", () => {
 
   it("renders userName and name", () => {
     const wrapper = mount(<Profile userName={"goodjam"} name={"JAEMO KOO"} />);
+
+    // props 접근
     expect(wrapper.props().userName).toBe("goodjam");
     expect(wrapper.props().name).toBe("JAEMO KOO");
 
-    // DOM에 우리가 원하는 텍스트가 나타나는지 확인하는 테스트 코드
+    // DOM 확인 (우리가 원하는 텍스트가 나타나는지 확인하는 테스트 코드)
     const boldElement = wrapper.find("b");
     expect(boldElement.contains("goodjam")).toBe(true);
     const spanElement = wrapper.find("span");
