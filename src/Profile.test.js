b8a38b8ba42a5e5c1331b3232bf4f9507ebfe0f9
JaemoKoo <devgoozz@gmail.com>
2023-04-04 23:10:13 +0900

    test: 스냅샷 테스트 코드 작성

--- src/Profile.test.js
+++ src/Profile.test.js
@@ -6,4 +6,16 @@ describe("<Profile />", () => {
     const wrapper = mount(<Profile userName={"goodjam"} name={"JAEMO KOO"} />);
     expect(wrapper).toMatchSnapshot();
   });
+
+  it("renders userName and name", () => {
+    const wrapper = mount(<Profile userName={"goodjam"} name={"JAEMO KOO"} />);
+    expect(wrapper.props().userName).toBe("goodjam");
+    expect(wrapper.props().name).toBe("JAEMO KOO");
+
+    // DOM에 우리가 원하는 텍스트가 나타나는지 확인하는 테스트 코드
+    const boldElement = wrapper.find("b");
+    expect(boldElement.contains("goodjam")).toBe(true);
+    const spanElement = wrapper.find("span");
+    expect(spanElement.text()).toBe("(JAEMO KOO)");
+  });
 });
