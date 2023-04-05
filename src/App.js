2d0bd608a3b73406f85c043079efc8e72fb2115b
JaeMo Koo <46645378+goodjam92@users.noreply.github.com>
2023-04-05 13:55:42 +0900

    Revert "Class component testing"

--- src/App.js
+++ src/App.js
@@ -1,12 +1,23 @@
-import "./App.css";
-import Counter from "./Counter";
-import Profile from "./Profile";
+import logo from './logo.svg';
+import './App.css';
 
 function App() {
   return (
-    <div>
-      <Profile userName={"goodjam"} name={"JAEMO KOO"} />
-      <Counter />
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
     </div>
   );
 }
