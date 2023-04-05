839fc9ff2c3319f8d1daf1732ecf7c5fe550160b
JaemoKoo <devgoozz@gmail.com>
2023-04-05 12:29:41 +0900

    feat: 클래스형 컴포넌트 테스팅을 위한 컴포넌트 추가

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import "./App.css";
+import Counter from "./Counter";
 import Profile from "./Profile";
 
 function App() {
   return (
     <div>
       <Profile userName={"goodjam"} name={"JAEMO KOO"} />
+      <Counter />
     </div>
   );
 }
