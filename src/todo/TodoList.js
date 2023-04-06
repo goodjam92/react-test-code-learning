14a8992c68c8fdd58683e2e48525cc69d3d9acb5
JaemoKoo <devgoozz@gmail.com>
2023-04-06 14:17:29 +0900

    feat: TodoList 컴포넌트 작성

---
+++ src/todo/TodoList.js
@@ -0,0 +1,19 @@
+import { useState } from "react";
+import TodoItem from "./TodoItem";
+
+const TodoList = ({ todos, onToggle, onRemove }) => {
+  return (
+    <ul>
+      {todos.map((todo) => (
+        <TodoItem
+          key={todo.id}
+          todo={todo}
+          onToggle={onToggle}
+          onRemove={onRemove}
+        />
+      ))}
+    </ul>
+  );
+};
+
+export default TodoList;
